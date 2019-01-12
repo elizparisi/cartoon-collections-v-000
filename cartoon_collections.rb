ce91f2f91b26b6fb2af9a45c5830feafdb931519
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-01-12 02:15:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  planeteer_calls.any? { |call| call.length > 4 }
+  calls.any? { |calls| calls.length > 4 }
 end
 
 def find_the_cheese(snacks)# code an argument here
